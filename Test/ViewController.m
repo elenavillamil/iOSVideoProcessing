15517ef452136b378073972eb297a89de0ca8954
jashook <jshook@smu.edu>
2014-12-13 16:36:38 -0600

    small changes, finds eyes now

--- Test/ViewController.m
+++ Test/ViewController.m
@@ -97,38 +97,78 @@ didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
     // Create a UIImage from the sample buffer data
     UIImage *image = [self imageFromSampleBuffer:sampleBuffer];
     
+    image = [UIImage imageWithCGImage:image.CGImage scale:image.scale orientation:UIImageOrientationDown];
+    
     //////////////////////
     
     UIView* window = ([UIApplication sharedApplication].delegate).window;
     EAGLContext *eaglContext = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
     
-    GLKView *videoPreviewView =[[GLKView alloc] initWithFrame:window.bounds context:eaglContext];
-    videoPreviewView.enableSetNeedsDisplay = NO;
+    //GLKView *videoPreviewView =[[GLKView alloc] initWithFrame:window.bounds context:eaglContext];
+    //videoPreviewView.enableSetNeedsDisplay = NO;
     
     // because the native video image from the back camera is in UIDeviceOrientationLandscapeLeft (i.e. the home button is on the right), we need to apply a clockwise 90 degree transform so that we can draw the video preview as if we were in a landscape-oriented view; if you're using the front camera and you want to have a mirrored preview (so that the user is seeing themselves in the mirror), you need to apply an additional horizontal flip (by concatenating CGAffineTransformMakeScale(-1.0, 1.0) to the rotation transform)
-    CGAffineTransform transform = CGAffineTransformMakeRotation(M_PI_2);
-    transform = CGAffineTransformConcat(transform, CGAffineTransformMakeScale(-1.0, 1.0));
+    //CGAffineTransform transform = CGAffineTransformMakeRotation(M_PI_2);
+    //transform = CGAffineTransformConcat(transform, CGAffineTransformMakeScale(-1.0, 1.0));
     
-    videoPreviewView.transform = transform;
-    videoPreviewView.frame = window.bounds;
+    //videoPreviewView.transform = transform;
+    //videoPreviewView.frame = window.bounds;
 
     CIContext* ci_context = [CIContext contextWithEAGLContext:eaglContext options:@{kCIContextWorkingColorSpace : [NSNull null]} ];
     
     //////////////////////
     
-    NSDictionary *opts = [NSDictionary dictionaryWithObjectsAndKeys:
+    int orientation = 1;
+    
+    switch (image.imageOrientation) {
+        case UIImageOrientationUp:
+            orientation = 1;
+            break;
+        case UIImageOrientationDown:
+            orientation = 3;
+            break;
+        case UIImageOrientationLeft:
+            orientation = 8;
+            break;
+        case UIImageOrientationRight:
+            orientation = 6;
+            break;
+        case UIImageOrientationUpMirrored:
+            orientation = 2;
+            break;
+        case UIImageOrientationDownMirrored:
+            orientation = 4;
+            break;
+        case UIImageOrientationLeftMirrored:
+            orientation = 5;
+            break;
+        case UIImageOrientationRightMirrored:
+            orientation = 7;
+            break;
+        default:
+            break;
+    }
+    
+    NSDictionary *detectorOptions = @{ CIDetectorAccuracy : CIDetectorAccuracyHigh };
+    CIDetector *face_detector = [CIDetector detectorOfType:CIDetectorTypeFace context:nil options:detectorOptions];
+    
+    NSArray *features = [face_detector featuresInImage:[CIImage imageWithCGImage:image.CGImage]
+                                              options:@{ CIDetectorEyeBlink : @YES,
+                                                         CIDetectorImageOrientation :[NSNumber numberWithInt:orientation] }];
+    
+    /*NSDictionary *opts = [NSDictionary dictionaryWithObjectsAndKeys:
                           CIDetectorImageOrientation,
                           [self ciOrientationFromDeviceOrientation:[UIApplication sharedApplication].statusBarOrientation],
                           [NSNumber numberWithBool:YES],
                           CIDetectorEyeBlink,
                           [NSNumber numberWithBool:YES],
                           CIDetectorSmile,
-                          nil];
+                          nil];*/
     
-    CIDetector *detector = [CIDetector detectorOfType:CIDetectorTypeFace context:ci_context options:opts];
+    //CIDetector *detector = [CIDetector detectorOfType:CIDetectorTypeFace context:ci_context options:opts];
     
     
-    NSArray* features = [detector featuresInImage:image.CIImage options:opts];
+    //NSArray* features = [face_detector featuresInImage:image.CIImage options:opts];
     
     for (CIFaceFeature *f in features)
     {
@@ -151,6 +191,7 @@ didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
 
     dispatch_async(dispatch_get_main_queue(), ^{
         [self.image_view setImage:image];
+        
     });
     
     //< Add your code here that uses the image >
